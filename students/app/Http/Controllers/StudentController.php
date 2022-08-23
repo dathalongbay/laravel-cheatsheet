aefaa88e89fab406e00813986b469e655c7c1c21
dat <dathalongbay@gmail.com>
2022-08-23 21:12:41 +0700

    111

---
+++ students/app/Http/Controllers/StudentController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    //
+    public function index() {
+        echo '<br>' . __METHOD__;
+    }
+
+    public function create() {
+        echo '<br>' . __METHOD__;
+    }
+
+    public function edit() {
+        echo '<br>' . __METHOD__;
+    }
+
+    public function show() {
+        echo '<br>' . __METHOD__;
+    }
+
+    public function store() {
+        echo '<br>' . __METHOD__;
+    }
+
+
+    public function update() {
+        echo '<br>' . __METHOD__;
+    }
+
+    public function destroy() {
+        echo '<br>' . __METHOD__;
+    }
+
+}
